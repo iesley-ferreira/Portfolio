e055346ce43311890117e66956515a2000d712ee
iesley-ferreira <iesley_ferreira@hotmail.com>
2024-04-03 21:23:50 -0300

    updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import './styles/Header.css';
+import React from "react";
+import "./styles/Header.css";
 
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 
 const Header: React.FC = () => {
   const location = useLocation();
   const path = location.pathname;
 
   return (
-    <header className='header-container'>
-      {path !== '/' ? (
-          <h1>IesleyFerreira.</h1>
-        ) : (
-          <h1>PORTFOLIO.</h1>
-        )}
+    <header className="header-container-principal">
+      <div className="header-container">
+        {path !== "/" ? <h1>IesleyFerreira.</h1> : <h1>PORTFOLIO.</h1>}
+      </div>
     </header>
   );
-}
+};
 
 export default Header;
